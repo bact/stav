85e1815e720bb3cc08ff19cae447089ac45ed4e1
Arthit Suriyawongkul <arthit@gmail.com>
2023-11-14 05:49:08 +0000

    Add more classes from US AIA and few metrics

--- stav/eu/aia/__init__.py
+++ stav/eu/aia/__init__.py
@@ -4,8 +4,10 @@ Vocabulary for accountability artifacts from EU AI Act (draft)
 
 __stav_namespace__ = "eu.aia"
 
+
 class TechnicalDocumentation(object):
     NAME = "technical_documentation"
+
     class Description(object):
         NAME = "description"
         INTENDED_PURPOSE = "intended_purpose"
@@ -17,8 +19,15 @@ class TechnicalDocumentation(object):
         INSTRUCTIONS_USAGE = "instructions_usage"
         INSTRUCTIONS_INSTALLATION = "instructions_installation"
 
-class Log(object):
+
+class LogRecord(object):
     NAME = "log_record"
+
+
+__all__ = [
+    "TechnicalDocumentation",
+    "LogRecord",
+]
 
 """
 eu.aia.TechnicalDocumentation
